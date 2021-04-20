b5ba923002b6a12968f31c35ac76efe25776694d
kishimototakato <kd1309731@st.kobedenshi.ac.jp>
2021-04-20 13:44:58 +0900

    ・テスト

---
+++ BaseFramework/Src/Application/Game/GameSystem.cpp
@@ -0,0 +1,22 @@
+﻿#include "GameSystem.h"
+
+void GameSystem::Init()
+{
+	//m_sky.Load("Data/Models/Sky/Sky.gltf");
+	OutputDebugStringA("Init\n");
+}
+
+void GameSystem::Update()
+{
+	OutputDebugStringA("Update\n");
+}
+
+void GameSystem::Draw()
+{
+	OutputDebugStringA("Draw\n");
+}
+
+void GameSystem::Release()
+{
+	OutputDebugStringA("Release\n");
+}
---
+++ BaseFramework/Src/Application/Game/GameSystem.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+class GameSystem
+{
+public:
+	GameSystem() {}		//コンストラクター
+	~GameSystem() { Release(); }	//デストラクター
+
+	void Init();		//初期化
+	void Update();		//更新
+	void Draw();		//描画
+
+
+private:
+	void Release();		//解放
+
+	KdModelWork m_sky;	//スカイスフィア
+};
+
